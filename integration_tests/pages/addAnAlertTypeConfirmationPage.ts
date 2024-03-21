422dfe66ae9464b265ac48c968ca8d054a0983b8
jbowdidge-moj <122450608+jbowdidge-moj@users.noreply.github.com>
2024-03-21 15:53:02 +0000

    AL-95 add confirmation page to create alert type flow (#9)

--- integration_tests/pages/addAnAlertTypeConfirmationPage.ts
+++ integration_tests/pages/addAnAlertTypeConfirmationPage.ts
@@ -1,7 +1,19 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class AddAnAlertTypeConfirmationPage extends Page {
   constructor() {
     super('Check your answers before creating your alert type')
+  }
+
+  public code(): PageElement {
+    return cy.get('[data-qa=code]')
+  }
+
+  public description(): PageElement {
+    return cy.get('[data-qa=description]')
+  }
+
+  public continue(): PageElement {
+    return cy.get('[data-qa=alert-type-code-save]')
   }
 }

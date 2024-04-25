3390a767b33a8e20572d547387552e885ae80f94
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-04-25 15:42:28 +0100

    Fix cypress test

--- integration_tests/pages/updateAlertTypeDescriptionSuccessPage.ts
+++ integration_tests/pages/updateAlertTypeDescriptionSuccessPage.ts
@@ -2,8 +2,6 @@ import Page from './page'
 
 export default class UpdateAlertTypeDescriptionSuccessPage extends Page {
   constructor(alertTypeCode: string, alertTypeDescription: string) {
-    super(
-      `Alert type code: <strong>${alertTypeCode}</strong> is updated with new description: <strong>${alertTypeDescription}</strong>.`,
-    )
+    super(`Alert type code: ${alertTypeCode} is updated with new description: ${alertTypeDescription}.`)
   }
 }

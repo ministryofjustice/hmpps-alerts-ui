dad09ed68692307746bb2118e8732bb4a6f99417
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-10-31 17:33:26 +0000

    Add js to homepage cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+import { nodeListForEach } from './utils'
+import Card from './card'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
+
+var $cards = document.querySelectorAll('.card--clickable')
+nodeListForEach($cards, function ($card) {
+  new Card($card)
+})

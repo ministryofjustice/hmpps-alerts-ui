2b6e41680c744c5ae01e875e453c4e1141129e56
James Reed <james.reed@digital.justice.gov.uk>
2024-10-31 13:02:34 +0000

    chore: remove trycatch

--- server/middleware/checkPopulateUserCaseloads.ts
+++ server/middleware/checkPopulateUserCaseloads.ts
@@ -1,20 +1,14 @@
 import { RequestHandler, Router } from 'express'
-import logger from '../../logger'
 
 export default function checkPopulateUserCaseloads(): RequestHandler {
   const router = Router()
 
   router.use(async (_req, res, next) => {
-    try {
-      if (res.locals.feComponentsMeta?.caseLoads) {
-        res.locals.user.caseloads = res.locals.feComponentsMeta.caseLoads
-      }
-      if (res.locals.feComponentsMeta?.activeCaseLoad) {
-        res.locals.user.activeCaseLoadId = res.locals.feComponentsMeta.activeCaseLoad.caseLoadId
-      }
-    } catch (error) {
-      logger.error(error, `Failed to get caseloads for: ${res.locals.user.username}`)
-      next(error)
+    if (res.locals.feComponentsMeta?.caseLoads) {
+      res.locals.user.caseloads = res.locals.feComponentsMeta.caseLoads
+    }
+    if (res.locals.feComponentsMeta?.activeCaseLoad) {
+      res.locals.user.activeCaseLoadId = res.locals.feComponentsMeta.activeCaseLoad.caseLoadId
     }
     next()
   })

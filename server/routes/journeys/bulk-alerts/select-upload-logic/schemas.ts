3390193c5d3b5ec45572fc1785acc5e71c117ceb
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-11-13 17:23:56 +0000

    Add /bulk-alerts/select-upload-logic screen

---
+++ server/routes/journeys/bulk-alerts/select-upload-logic/schemas.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+import { createSchema } from '../../../../middleware/validationMiddleware'
+import { BulkAlertJourney } from '../../../../@types/express'
+
+const ERROR_MSG = 'You must select one option'
+
+export const schema = createSchema({
+  cleanupMode: z
+    .string({ message: ERROR_MSG })
+    .refine(val => ['KEEP_ALL', 'EXPIRE_FOR_PRISON_NUMBERS_NOT_SPECIFIED'].includes(val), ERROR_MSG),
+})
+
+export type SchemaType = { cleanupMode: Exclude<BulkAlertJourney['cleanupMode'], undefined> }

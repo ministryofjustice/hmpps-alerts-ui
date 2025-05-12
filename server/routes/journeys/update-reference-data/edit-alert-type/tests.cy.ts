100c9a95e4fc2ab27ab46f2ff40fe08b517cfcc1
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-05-12 09:57:20 +0100

    Add edit alert type journey pages (#196)

--- server/routes/journeys/update-reference-data/edit-alert-type/tests.cy.ts
+++ server/routes/journeys/update-reference-data/edit-alert-type/tests.cy.ts
@@ -2,11 +2,11 @@ import { v4 as uuidV4 } from 'uuid'
 import AuthorisedRoles from '../../../../authentication/authorisedRoles'
 import injectJourneyDataAndReload from '../../../../../integration_tests/utils/e2eTestUtils'
 
-context('test /update-reference-data/add-alert-code screen', () => {
+context('test /update-reference-data/add-alert-type screen', () => {
   const uuid = uuidV4()
 
   const getContinueButton = () => cy.findByRole('button', { name: /Continue/ })
-  const getDescriptionInput = () => cy.findByRole('textbox', { name: /Enter a new alert description$/ })
+  const getDescriptionInput = () => cy.findByRole('textbox', { name: /Enter a new alert type description$/ })
 
   beforeEach(() => {
     cy.task('reset')
@@ -17,7 +17,7 @@ context('test /update-reference-data/add-alert-code screen', () => {
 
   it('should try out all cases', () => {
     navigateToTestPage()
-    cy.url().should('to.match', /\/edit-alert-code$/)
+    cy.url().should('to.match', /\/edit-alert-type$/)
     validatePageContents()
     validateErrorMessages()
     proceedToNextPage()
@@ -29,41 +29,36 @@ context('test /update-reference-data/add-alert-code screen', () => {
     cy.visit(`/${uuid}/update-reference-data`, { failOnStatusCode: false })
     injectJourneyDataAndReload(uuid, {
       updateRefData: {
-        referenceDataType: 'ALERT_CODE',
+        referenceDataType: 'ALERT_TYPE',
         changeType: 'EDIT_DESCRIPTION',
         alertType: {
           code: 'AB',
-          description: 'Type Name',
-          isActive: true,
-        },
-        alertCode: {
-          code: 'AA',
           description: 'Original Name',
           isActive: true,
         },
       },
     })
-    cy.visit(`/${uuid}/update-reference-data/edit-alert-code`, { failOnStatusCode: false })
+    cy.visit(`/${uuid}/update-reference-data/edit-alert-type`, { failOnStatusCode: false })
   }
 
   const validatePageContents = () => {
-    cy.title().should('match', /Edit alert description - Maintain alerts reference data - DPS/)
+    cy.title().should('match', /Edit alert type description - Maintain alerts reference data - DPS/)
     cy.findByRole('heading', {
-      name: /Edit alert description/,
+      name: /Edit alert type description/,
     }).should('be.visible')
     getContinueButton().should('be.visible')
     getDescriptionInput().should('be.visible').and('have.value', 'Original Name')
     cy.findByRole('link', { name: /^Back$/ })
       .should('be.visible')
       .and('have.attr', 'href')
-      .and('match', /select-alert-code$/)
+      .and('match', /select-alert-type$/)
   }
 
   const validateErrorMessages = () => {
     getDescriptionInput().clear()
     getContinueButton().click()
 
-    cy.findByRole('link', { name: /An alert description must be between 1 and 40 characters$/ })
+    cy.findByRole('link', { name: /An alert type description must be between 1 and 40 characters$/ })
       .should('be.visible')
       .click()
     getDescriptionInput().should('be.focused')

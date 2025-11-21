ff7b9545d258f707fca6147939746b72a9f3bedb
Christopher Moss <chris.moss1@justice.gov.uk>
2025-11-21 08:48:42 +0000

    Copy njk files from server/views to dist/views on change (hmpps-template-typescript#621)

--- esbuild/build.config.js
+++ esbuild/build.config.js
@@ -23,6 +23,7 @@ const getBuildConfig = () => {
         {
           from: path.join(cwd, 'server/views/**/*'),
           to: path.join(cwd, 'dist/server/views'),
+          watch: isWatchMode,
         },
         {
           from: path.join(cwd, 'server/routes/**/*'),
@@ -38,6 +39,7 @@ const getBuildConfig = () => {
         {
           from: path.join(cwd, 'assets/images/**/*'),
           to: path.join(cwd, 'dist/assets/images'),
+          watch: isWatchMode,
         },
         {
           from: path.join(cwd, 'assets/downloads/**/*'),

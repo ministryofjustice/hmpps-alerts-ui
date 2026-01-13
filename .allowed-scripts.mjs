342d6d13193b896c8ad4f0de52d1b05d443c0a28
Igor <igor.ushkarev1@justice.gov.uk>
2026-01-13 13:03:33 +0000

    Remove direct `sass` dependency (it should be installed via `esbuild-sass-plugin`)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,14 +2,18 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by esbuild for watching files during development
+    'node_modules/@parcel/watcher@2.5.4': 'ALLOW',
     // Enables use of Sentry CLI:
-    'node_modules/@sentry/cli@2.58.0': 'ALLOW',
+    'node_modules/@sentry/cli@2.58.4': 'ALLOW',
     // Needed for running integration tests:
     'node_modules/cypress@15.8.2': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
     'node_modules/esbuild@0.27.2': 'ALLOW',
+    // Needed by esbuild (via chokidar)
+    'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },

4c394d26e032fa55cb607fc8009088e005b6f5b6
Igor <igor.ushkarev1@justice.gov.uk>
2026-05-28 12:14:19 +0100

    Add missing journey ID middleware tests

--- server/middleware/insertJourneyIdentifier.ts
+++ server/middleware/insertJourneyIdentifier.ts
@@ -1,13 +1,14 @@
 import { randomUUID } from 'node:crypto'
-import type { NextFunction, Request, Response } from 'express'
+import type { RequestHandler } from 'express'
 import { validateUuid } from '../utils/validateUuid'
 
-export default function insertJourneyIdentifier() {
-  return (req: Request, res: Response, next: NextFunction): void => {
+export default function insertJourneyIdentifier(): RequestHandler {
+  return (req, res, next): void => {
     const uuid = req.url.split('/')[1] || '/'
     if (!validateUuid(uuid)) {
-      return res.redirect(`${req.baseUrl}/${randomUUID()}${req.url}`)
+      res.redirect(`${req.baseUrl}/${randomUUID()}${req.url}`)
+      return
     }
-    return next()
+    next()
   }
 }
